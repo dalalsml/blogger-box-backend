df599f38167efa4bc4ac56b99194eddc21b5748c
DALAL365 <daloulasmail@gmail.com>
2026-04-16 17:02:33 +0200

    add post repository layer with JPA

--- src/main/java/com/dauphine/blogger/repositories/PostRepository.java
+++ src/main/java/com/dauphine/blogger/repositories/PostRepository.java
@@ -3,7 +3,16 @@ package com.dauphine.blogger.repositories;
 import com.dauphine.blogger.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
+    List<Post> findByCategoryIdOrderByCreatedDateDesc(UUID categoryId);
+
+    List<Post> findByCreatedDateBetween(LocalDateTime start, LocalDateTime end);
+
+    List<Post> findAllByOrderByCreatedDateDesc();
+
+    List<Post> findByCreatedDateBetweenOrderByCreatedDateDesc(LocalDateTime start, LocalDateTime end);
 }
